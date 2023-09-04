3f496f0b73615208d067465f8cfcf17bd5a679de
Eligio Biasi <ex_98@live.it>
2023-09-04 15:57:52 +0200

    test n2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+                
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -16,6 +16,9 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+                <h3>
+                    test 2
+                </h3>
             </div>
         </div>
     </div>
