997e851217f1cfca8d01e6cc46d193920ec7536d
GabrieleGrossi <grossigabriele98@gmail.com>
2023-09-04 16:20:49 +0200

    setup project

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,5 @@ use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Auth::routes();
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function(){
-    Route::get('/',[AdminHomeController::class, 'home'])->name('home');
-    Route::resource('/projects', ProjectController::class);
-});
-Route::name('guest.')->group(function(){
-    Route::get('/',[GuestHomeController::class, 'home'])->name('home');
-});
 
 // prova per vedere se funziona tutto correttamente
