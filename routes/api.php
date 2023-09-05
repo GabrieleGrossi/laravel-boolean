409fbed8d80621d49c42b89d9c5eb16407d1afd9
Eligio Biasi <ex_98@live.it>
2023-09-05 09:12:56 +0200

    added "use"folder path to api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\ApiBarController as ApiBarController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/projects', [ApiBarController::class, 'index'])->name('api.bar.index');
+Route::get('/cocktail', [ApiBarController::class, 'index'])->name('api.bar.index');
